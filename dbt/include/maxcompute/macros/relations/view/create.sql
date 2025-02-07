345dab9e6b87630763f1544ef617b53d97f0f914
鼎昕 <zhangdingxin.zdx@alibaba-inc.com>
2025-02-07 09:59:37 +0800

    refactor: 在创建视图时添加 "or replace" 关键字，以确保在视图已存在的情况下能够成功更新

--- dbt/include/maxcompute/macros/relations/view/create.sql
+++ dbt/include/maxcompute/macros/relations/view/create.sql
@@ -2,7 +2,7 @@
   {%- set sql_header = config.get('sql_header', none) -%}
 
   {{ sql_header if sql_header is not none }}
-  create view {{ relation.render() }}
+  create or replace view {{ relation.render() }}
     {% set contract_config = config.get('contract') %}
     {% if contract_config.enforced %}
       {{ get_assert_columns_equivalent(sql) }}
